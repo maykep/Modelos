4ed6aa75f9e9d3bdb1a0a5a094a455d2ec330446
mayke <maykepaiva.mgap@gmail.com>
2020-03-27 10:02:08 -0300

    Back-End da Aplicação

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,21 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async create(request,response){
+        const {id} = request.body;
+
+        if(id == null)
+        {
+            return response.status(400).json({error:"ID is Null"});
+        }
+        const ong = await connection('ongs')
+        .where('id',id)
+        .select('name')
+        .first();
+
+        if(!ong){
+            return response.status(400).json({error:"No ONG found with this ID"});
+        }
+        return response.json(ong);
+    }
+}
